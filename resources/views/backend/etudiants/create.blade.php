45dfad5b4f433bd01e6032849390c7ae4dc79c90
Barnabe TEBDA <barnabe.tebda@gmail.com>
2023-09-26 00:04:36 +0000

    Ajout de views des autres tables concernees

--- resources/views/backend/etudiants/create.blade.php
+++ resources/views/backend/etudiants/create.blade.php
@@ -85,6 +85,16 @@
                             </div>
                         </div>
                         
+                        <div class="row py-4">
+                            <label class="col-sm-2 col-form-label"></label>
+                            <div class="col">
+                                <button type=" submit button" class="btn btn-success">Enregsitrer</button>
+                            </div>
+                            <div class="col">
+                                <a href="{{ route('etudiants.index') }}"> <button type="button" class="btn btn-danger">Annuler</button> </a>
+                            </div>
+
+                        </div>
                    
                         
                     </form>
