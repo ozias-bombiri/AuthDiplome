6a3ce9636faae7c32957cfeed319cf8932b27250
Barnabe TEBDA <barnabe.tebda@gmail.com>
2023-10-03 20:31:26 +0000

    Ajout de datatable sans package pour niveaux_etudes

--- resources/views/backend/niveau_etudes/index.blade.php
+++ resources/views/backend/niveau_etudes/index.blade.php
@@ -2,7 +2,6 @@
 
 
 @section('contenu')
-<<<<<<< HEAD
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
@@ -13,20 +12,9 @@
                 <a class="btn btn-success" href="{{ route('niveau_etudes.create') }}"> Ajouter</a>
              
             </div>
-=======
-
-<div class="card">
-    <div class="card-header">{{ __('Niveau d\'étude') }}</div>
-
-    <div class="card-body">
-
-        <div class="table-responsive">
-            
->>>>>>> bozi2
         </div>
     </div>
 
-<<<<<<< HEAD
 
     @if ($message = Session::get('success'))
         <div class="alert alert-success">
@@ -35,41 +23,36 @@
     @endif
 
 
-    <table class="table table-bordered">
-        <tr>
-            <th>No</th>
-            <th>Name</th>
-            <th>Details</th>
-            <th width="280px">Action</th>
-        </tr>
-	    @foreach ($niveaux as $niveau)
-	    <tr>
-	        <td>{{ ++$i }}</td>
-	        <td>{{ $niveau->intitule }}</td>
-	        <td>{{ $niveau->description }}</td>
-	        <td>
-                <form action="{{ route('niveau_etudes.destroy',$niveau->id) }}" method="POST">
-                    <a class="btn btn-info" href="{{ route('niveau_etudes.show',$niveau->id) }}">Show</a>  
-                    <a class="btn btn-primary" href="{{ route('niveau_etudes.edit',$niveau->id) }}">Edit</a>
-                   
-                    @csrf
-                    @method('DELETE')
+    <table id="data" class="table table-bordered">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Name</th>
+                <th>Details</th>
+                <th width="280px">Action</th>
+            </tr>
+        </thead>
+        <tbody>            
+            @foreach ($niveaux as $niveau)
+            <tr>
+                <td>{{ ++$i }}</td>
+                <td>{{ $niveau->intitule }}</td>
+                <td>{{ $niveau->description }}</td>
+                <td>
+                    <form action="{{ route('niveau_etudes.destroy',$niveau->id) }}" method="POST">
+                        <a class="btn btn-info" href="{{ route('niveau_etudes.show',$niveau->id) }}">Show</a>  
+                        <a class="btn btn-primary" href="{{ route('niveau_etudes.edit',$niveau->id) }}">Edit</a>
                     
-                    <button type="submit" class="btn btn-danger">Delete</button>
-                  
-                </form>
-	        </td>
-	    </tr>
+                        @csrf
+                        @method('DELETE')
+                        
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    
+                    </form>
+                </td>
+            </tr>
 	    @endforeach
-    </table>
-
-
-   
-
+    </tbody>
+</table>
 
 @endsection
-=======
-@push('costum-scripts')
-
-@endpush
->>>>>>> bozi2
